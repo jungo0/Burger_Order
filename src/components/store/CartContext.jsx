4b4b6a3912731199df9636905e3e5fce0bcdb990
jungo <81670100+jungo0@users.noreply.github.com>
2024-04-01 20:42:28 +0900

    feat: 장바구니 reducer

---
+++ src/components/store/CartContext.jsx
@@ -0,0 +1,82 @@
+import { createContext, useReducer } from "react";
+
+//  const [state, dispatch] = useReducer(reducer, initialState);
+
+const CartContext = createContext({
+  items: [],
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    // 해당 항목이 이미 배열에 있으면
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const updatedItems = [...state.items]; // 기존 아이템 복사
+
+    if (existingCartItemIndex > -1) {
+      // 이미 장바구니에 있는 아이템인 경우
+      const existingItem = state.items[existingCartItemIndex];
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1, // 수량 증가
+      };
+      updatedItems[existingCartItemIndex] = updatedItem; // 업데이트된 아이템으로 교체
+    } else {
+      updatedItems.push({ ...action.item, quantity: 1 }); // 새로운 아이템 추가
+    }
+
+    return { ...state, items: updatedItems }; // 업데이트된 상태 반환
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedItems = [...state.items];
+
+    //1개만 아예 장바구니에서 삭제
+    if (existingCartItem.quantity === 1) {
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  return state;
+}
+
+export function CartContextProvider({ children }) {
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
+
+  function addItem(item) {
+    dispatchCartAction({ type: "ADD_ITEM", item });
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({ type: "REMOVE_ITEM", id });
+  }
+
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
+}
+
+export default CartContext;
